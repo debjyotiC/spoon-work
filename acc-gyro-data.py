ab8b5579860e1914066248d5cd8383cb299be8f4
debjyotiC <djbabai.debjyoti@gmail.com>
2021-01-24 13:10:57 +0530

    data and updated code added

--- acc-gyro-data.py
+++ acc-gyro-data.py
@@ -1,6 +1,8 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+alpha = 0.9
 
 acc_df = pd.read_csv('data/acc.csv').drop(['Timestamp'], axis=1)
 gyro_df = pd.read_csv('data/gyro.csv').drop(['Timestamp'], axis=1)
@@ -15,8 +17,16 @@ gyro_y = np.array(gyro_df['Y'])
 gyro_z = np.array(gyro_df['Z'])
 gyro_time = np.array(gyro_df['Milliseconds'])
 
+dt = (gyro_df['Milliseconds'][1] - gyro_df['Milliseconds'][0]) * pow(10, -3)
+acc_angle = np.arctan(np.divide(acc_x, np.add(np.square(acc_y), np.square(acc_z))))
+angle = np.degrees((1 / alpha) * ((1 - alpha) * gyro_x * dt + alpha * acc_angle))
 
-acc_angle = -np.arctan(np.divide(acc_x, np.add(np.square(acc_y), np.square(acc_z))))
+
+# save data
+values = {'Time': acc_time, 'Acc_x': acc_x, 'Acc_y': acc_y, 'Acc_z': acc_z, 'Gyro_x': gyro_x, 'Gyro_y': gyro_y,
+          'Gyro_z': gyro_z, 'Filter_angle': angle}
+df_w = pd.DataFrame(values, columns=['Time', 'Acc_x', 'Acc_y', 'Acc_z', 'Gyro_x', 'Gyro_y', 'Gyro_z', 'Filter_angle'])
+df_w.to_csv("data/final-data.csv", index=None, header=True)
 
 
 fig, axs = plt.subplots(3)
@@ -34,8 +44,8 @@ axs[1].legend()
 axs[1].set_ylabel('Gyro (degree/s)')
 plt.xlabel('Time (ms)')
 
-axs[2].plot(acc_time, acc_angle, label='Filter data')
+axs[2].plot(acc_time, angle, color='red', label=f'Filter data with dt={dt}')
 axs[2].legend()
-axs[2].set_ylabel('Angle (rad)')
+axs[2].set_ylabel('Angle (degree)')
 plt.xlabel('Time (ms)')
 plt.show()
